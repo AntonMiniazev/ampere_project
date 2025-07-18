9387d65bc053d98a1d6933797f8bacde1829ad8a
AntonMiniazev <minyazevae@outlook.com>
2025-07-18 15:57:36 +0200

    Added bootstrap.sh

---
+++ kube-cluster/provision/bootstrap.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+# Update system
+apt-get update
+apt-get upgrade -y
+
+# Install Docker
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common gnupg lsb-release
+curl -fsSL https://get.docker.com | sh
+usermod -aG docker vagrant
+
+# Install Kubernetes tools
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+
+# Turn off swap (K8s requirement)
+swapoff -a
+sed -i '/ swap / s/^/#/' /etc/fstab
+
+# Enable bridged networking for K8s networking
+modprobe br_netfilter
+cat <<EOF | tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
