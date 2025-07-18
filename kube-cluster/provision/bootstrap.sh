fc15a0d51753ced924c613710d5f4f1b1e5916dd
AntonMiniazev <minyazevae@outlook.com>
2025-07-18 17:33:21 +0200

    Added bootstrap.sh

--- kube-cluster/provision/bootstrap.sh
+++ kube-cluster/provision/bootstrap.sh
@@ -6,7 +6,7 @@ apt-get update -y
 apt-get upgrade -y
 
 echo ">> Installing base utilities"
-apt-get install -y curl vim net-tools htop lsb-release apt-transport-https ca-certificates gnupg sudo
+apt-get install -y curl vim net-tools htop lsb-release apt-transport-https ca-certificates gnupg sudo openssh-server
 
 echo ">> Disabling swap"
 swapoff -a
@@ -15,11 +15,22 @@ sed -i '/ swap / s/^/#/' /etc/fstab
 echo ">> Appending hostname to /etc/hosts"
 echo "127.0.1.1 $(hostname)" >> /etc/hosts
 
-echo ">> Enabling password authentication over SSH"
-sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+echo ">> Creating .ssh directory and copying public keys"
+mkdir -p /home/vagrant/.ssh
+cat /home/vagrant/host_ssh/authorized_keys > /home/vagrant/.ssh/authorized_keys
+
+chmod 700 /home/vagrant/.ssh
+chmod 600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant:vagrant /home/vagrant/.ssh
+
+echo ">> Disabling password authentication in SSH"
+sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config
+sed -i 's/^#\?PubkeyAuthentication.*/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+
+echo ">> Restarting SSH service"
 systemctl restart ssh
 
-echo ">> Setting timezone to Europe/Belgrade"
+echo ">> Setting timezone"
 timedatectl set-timezone Europe/Belgrade
 
 echo ">> Done"
