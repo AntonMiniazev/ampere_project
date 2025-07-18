fcdc8e80827fba0d7638e59f8eb7e56a8c035edc
AntonMiniazev <minyazevae@outlook.com>
2025-07-18 16:19:48 +0200

    Added bootstrap.sh

--- kube-cluster/provision/bootstrap.sh
+++ kube-cluster/provision/bootstrap.sh
@@ -12,7 +12,7 @@ usermod -aG docker vagrant
 
 # Install Kubernetes tools
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+echo "deb https://apt.kubernetes.io/ kubernetes-jammy main" > /etc/apt/sources.list.d/kubernetes.list
 
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
