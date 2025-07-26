7e744c6c2095eea4db38602fe9c169229be0d4e8
AntonMiniazev <minyazevae@outlook.com>
2025-07-26 21:42:16 +0200

    Added creds for airflow

--- dags/test_empty_dag.py
+++ dags/test_empty_dag.py
@@ -1,14 +1,13 @@
-# dags/test_empty_dag.py
-
 from airflow import DAG
+from airflow.operators.empty import EmptyOperator
 from datetime import datetime
 
 with DAG(
     dag_id="test_empty_dag",
     start_date=datetime(2023, 1, 1),
-    schedule_interval=None,
+    schedule=None,
     catchup=False,
     description="A test DAG that does nothing",
     tags=["test"],
 ) as dag:
-    pass
+    EmptyOperator(task_id="noop")
