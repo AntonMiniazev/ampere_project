39908c44ee54f9f1ab601a3cff292d0a8cdcbcb3
AntonMiniazev <minyazevae@outlook.com>
2025-12-21 17:31:41 +0100

    Updated test dag

---
+++ dags/my_dags/test_postgres_dag.py
@@ -0,0 +1,53 @@
+from utils.messages import build_client_message
+from airflow import DAG
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+import sys
+
+# Append the repo path to PYTHONPATH if needed
+sys.path.append("/opt/airflow/dags/repo")
+
+# Import the external helper function
+
+DAG_ID = "test_postgres_connection"
+
+
+def print_sql_result(**context):
+    result = context["ti"].xcom_pull(task_ids="test")
+    print("🧪 Raw XCom:", result)
+
+    # Handle case: list of rows
+    if isinstance(result, list) and len(result) > 0:
+        row = result[0]
+    else:
+        row = result
+
+    message = build_client_message(row)
+    print("🟢", message)
+
+
+with DAG(
+    DAG_ID,
+    schedule="@daily",
+    start_date=datetime(2025, 8, 1),
+    tags=["test"],
+    catchup=False,
+) as dag:
+    # Execute a SQL query to get the latest order
+    get_orders = SQLExecuteQueryOperator(
+        task_id="test",
+        conn_id="postgres_odbc_conn",  # type=ODBC
+        sql="SELECT * FROM apere_db.public.test_table",
+        do_xcom_push=True,
+        return_last=True,
+    )
+
+    # Print a message based on the SQL result
+    print_result = PythonOperator(
+        task_id="print_result",
+        python_callable=print_sql_result,
+    )
+
+    # Define task execution order
+    get_orders >> print_result
