74e0d6dd8d8ff32c5098fe1ef614dc475a2b5b5d
AntonMiniazev <minyazevae@outlook.com>
2026-01-24 14:49:13 +0100

    Testing raw to bronze ETL

--- dags/my_dags/ampere__bronze__landing_to_delta__daily.py
+++ dags/my_dags/ampere__bronze__landing_to_delta__daily.py
@@ -48,19 +48,19 @@ DRIVER_CORES = int(Variable.get("spark_driver_cores", default_var="1"))
 DRIVER_CORE_REQUEST = Variable.get(
     "spark_driver_core_request", default_var="250m"
 )
-DRIVER_MEMORY = Variable.get("spark_driver_memory", default_var="320m")
+DRIVER_MEMORY = Variable.get("spark_driver_memory", default_var="512m")
 DRIVER_MEMORY_OVERHEAD = Variable.get(
-    "spark_driver_memory_overhead", default_var="128m"
+    "spark_driver_memory_overhead", default_var="64m"
 )
 EXECUTOR_CORES = int(Variable.get("spark_executor_cores", default_var="1"))
 EXECUTOR_CORE_REQUEST = Variable.get(
     "spark_executor_core_request", default_var="250m"
 )
-EXECUTOR_MEMORY = Variable.get("spark_executor_memory", default_var="320m")
+EXECUTOR_MEMORY = Variable.get("spark_executor_memory", default_var="512m")
 EXECUTOR_MEMORY_OVERHEAD = Variable.get(
-    "spark_executor_memory_overhead", default_var="128m"
+    "spark_executor_memory_overhead", default_var="64m"
 )
-EXECUTOR_INSTANCES = int(Variable.get("spark_executor_instances", default_var="2"))
+EXECUTOR_INSTANCES = int(Variable.get("spark_executor_instances", default_var="1"))
 EVENT_LOOKBACK_DAYS = int(Variable.get("spark_event_lookback_days", default_var="2"))
 MAX_ACTIVE_TASKS = int(
     Variable.get("spark_raw_to_bronze_max_active_tasks", default_var="4")
